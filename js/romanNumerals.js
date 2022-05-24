bb6b7d1946cc6e7d6eb4cc7f05369d8ba236fc2b
KevinBaoB <kevinbaobui@gmail.com>
2022-05-24 11:06:14 -0500

    First

--- js/romanNumerals.js
+++ js/romanNumerals.js
@@ -1,3 +1,26 @@
-exports.toRoman = function(num) {
+// exports.toRoman = function(num) {
 
-};
+// };
+function toRoman(num) {
+  let list = {
+    1: "I",
+    2: "II",
+    3: "III",
+    5: "V",
+    10: "X",
+    50: "L",
+    100: "C",
+    500: "D",
+    1000: "M",
+    4: "IV",
+    9: "IX",
+    14: "XIV",
+    44: "XLIV",
+    944: "CMXLIV",
+  };
+  return list[num];
+}
+
+console.log(toRoman(1));
+console.log(toRoman(3));
+console.log(toRoman(4));
